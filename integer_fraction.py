6cbbc5e235aec5cbfab0cf28b6cb3d97896ab1a1
chica812 <chicasharma000@gmail.com>
2025-12-25 11:41:03 +0530

    fundamentals part 1 added

---
+++ integer_fraction.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a decimal number: "))
+
+integer_part = int(num)
+fractional_part = num - integer_part
+
+print("Integer part:", integer_part)
+print("Fractional part:", fractional_part)
